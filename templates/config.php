cdcb0bb508db26f2fad88f97137357ec2b7713fc
Steven Harradine <stevenharradine@gmail.com>
2016-06-23 13:08:36 -0400

    initial commit

---
+++ templates/config.php
@@ -0,0 +1,10 @@
+<?php
+	$username = '{{ omniture_to_pagerduty_omniture_username }}';
+	$password = '{{ omniture_to_pagerduty_omniture_password }}';
+	$reportSuiteID = '{{ omniture_to_pagerduty_omniture_reportSuiteID }}';
+	$useCache = false;
+	$alert_threshold = 100;
+	$pagerduty_api_accesskey = '{{ omniture_to_pagerduty_omniture_pagerduty_api_accesskey }}';
+	$pagerduty_servicekey = '{{ omniture_to_pagerduty_omniture_pagerduty_servicekey }}';
+	$cache_incidents = '{{ omniture_to_pagerduty_omniture_cache_incidents }}';
+	$cache_realtime_normal = '{{ omniture_to_pagerduty_omniture_cache_realtime_normal }}';
